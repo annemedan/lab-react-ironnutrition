a8f9f1b541055595e2a362898fa8a44607bba5f1
Anne Dantas <annedantas@gmail.com>
2021-12-01 23:13:57 +0000

    the page renders if the opposite is in place, but if the foods array is empty it's still not changing

--- src/components/EmptyList.js
+++ src/components/EmptyList.js
@@ -1,9 +1,10 @@
+import noFood from "./../msg28111671-721020.jpg";
 function EmptyList() {
   return (
     <div>
       <p> There's no more food content to be displayed. </p>
       <br />
-      <img src="./../../public/msg28111671-721020.jpg" alt="no more food" />
+      <img src={noFood} alt="no more food" width="50%" />
     </div>
   );
 }
